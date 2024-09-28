dd400596112d3fe70f0e4531ee3bec6b97c16630
jamesatomc <jiraphonwansai2002@gmail.com>
2024-09-28 15:51:16 +0700

    Initial Commit

--- app/src/main/java/com/example/smoothie/Menu.java
+++ app/src/main/java/com/example/smoothie/Menu.java
@@ -33,21 +33,21 @@ public class Menu extends AppCompatActivity {
 
         if ("smoothie".equals(menuType)) {
             menu menu1 = new menu();
-            menu1.setMenu_name("ชาเขียว");
+            menu1.setMenu_name("Green Tea");
             menu1.setMenu_image("https://i.pinimg.com/564x/19/1c/87/191c87753eff6e47a0d084c08fa695be.jpg");
             menu1.setMenu_detail("เมนูน้ำ");
             menu1.setMenu_price("50");
             menu.add(menu1);
 
             menu menu2 = new menu();
-            menu2.setMenu_name("กาแฟ");
+            menu2.setMenu_name("Coffee");
             menu2.setMenu_image("https://i.pinimg.com/564x/92/af/32/92af32f5eb84b559b42d9e215ef1b3b1.jpg");
             menu2.setMenu_detail("เมนูน้ำ");
             menu2.setMenu_price("30");
             menu.add(menu2);
 
             menu menu3 = new menu();
-            menu3.setMenu_name("นมปั่น");
+            menu3.setMenu_name("Milk Shake");
             menu3.setMenu_image("https://i.pinimg.com/564x/b7/49/8b/b7498b81634d92b04ff158a4963b5756.jpg");
             menu3.setMenu_detail("เมนูน้ำ");
             menu3.setMenu_price("55");
@@ -55,21 +55,21 @@ public class Menu extends AppCompatActivity {
 
         } else if ("food".equals(menuType)) {
             menu menu1 = new menu();
-            menu1.setMenu_name("ซาลาเปาทอดไส้ไข่หมูสับ");
+            menu1.setMenu_name("Fried buns with minced pork egg");
             menu1.setMenu_image("https://i.pinimg.com/564x/37/8d/4a/378d4aa07cf39e28828cfebd936cd759.jpg");
             menu1.setMenu_detail("เมนูอาหาร");
             menu1.setMenu_price("30");
             menu.add(menu1);
 
             menu menu2 = new menu();
-            menu2.setMenu_name("สปาเก็ตตี้ผัดขี้เมา");
+            menu2.setMenu_name("Spaghetti With Spicy");
             menu2.setMenu_image("https://i.pinimg.com/564x/31/16/08/31160834c28e6c10133936f6915b2843.jpg");
             menu2.setMenu_detail("เมนูอาหาร");
             menu2.setMenu_price("40");
             menu.add(menu2);
 
             menu menu3 = new menu();
-            menu3.setMenu_name("แซนวิช");
+            menu3.setMenu_name("Sandwich");
             menu3.setMenu_image("https://i.pinimg.com/564x/b9/0d/96/b90d969897c9cab8a1c24160b1e13b02.jpg");
             menu3.setMenu_detail("เมนูอาหาร");
             menu3.setMenu_price("50");
@@ -77,21 +77,21 @@ public class Menu extends AppCompatActivity {
 
         } else if ("dessert".equals(menuType)) {
             menu menu1 = new menu();
-            menu1.setMenu_name("แพนเค้ก ");
+            menu1.setMenu_name("Pancake");
             menu1.setMenu_image("https://i.pinimg.com/564x/8d/f7/3d/8df73d03c77684b283d24504b9627984.jpg");
             menu1.setMenu_detail("เมนของหวาน");
             menu1.setMenu_price("50");
             menu.add(menu1);
 
             menu menu2 = new menu();
-            menu2.setMenu_name("เค้ก");
+            menu2.setMenu_name("Cake");
             menu2.setMenu_image("https://i.pinimg.com/736x/ad/0e/d7/ad0ed7305702e1d0397b2312aafe01e0.jpg");
             menu2.setMenu_detail("เมนของหวาน");
             menu2.setMenu_price("55");
             menu.add(menu2);
 
             menu menu3 = new menu();
-            menu3.setMenu_name("ไอศกรีมโฮมเมด");
+            menu3.setMenu_name("Homemade ice cream");
             menu3.setMenu_image("https://i.pinimg.com/736x/11/4c/63/114c638dceaf1c640004c9307af18e85.jpg");
             menu3.setMenu_detail("เมนของหวาน");
             menu3.setMenu_price("30");
